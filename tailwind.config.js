9c00a427dc18c71a6f90d0b2b13a499d14a452d0
Wali Ullah Milton <waliullahmiltoncse@gmail.com>
2024-06-24 17:32:03 +0600

    navbar add

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'container': '1024px',
+      },
+      fontFamily:{
+        "jet" : ["JetBrains Mono","monospace"]
+      }
+    },
   },
   plugins: [],
 }
